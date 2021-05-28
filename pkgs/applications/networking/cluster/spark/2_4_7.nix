8c14b8609cbc9252e676eaa9e2060e19e5ef547a
Pavel Chuprikov <pschuprikov@gmail.com>
2021-05-28 15:03:06 +0200

    fix spark src hash

--- pkgs/applications/networking/cluster/spark/2_4_7.nix
+++ pkgs/applications/networking/cluster/spark/2_4_7.nix
@@ -1,5 +1,5 @@
 import ./common.nix {
-  version = "2.4.4";
-  sha256 = "0lkzlzidlqnp7js74dvydbizw844zyzl0gkgx6rh413s8xw219hm";
+  version = "2.4.7";
+  sha256 = "sha256-sEU8oHWHERPYNg9lgzLYNR3uyCAOJGMpziGoMuw5fpM=";
   depsSha256 = "";
 }
